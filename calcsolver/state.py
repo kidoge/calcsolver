23b1ffcfe84db09883015eff53fba3f4f422ad73
Kidon Son <kidonson@gmail.com>
2017-08-08 18:26:21 -0400

    Added steps to State

--- calcsolver/state.py
+++ calcsolver/state.py
@@ -1,8 +1,16 @@
 class State:
   _value = 0
+  _steps = []
   
-  def __init__(self, value):
+  def __init__(self, value=0, steps=[]):
     self._value = value
+    self._steps = steps
+
+  def clone(self):
+    return State(_value, _steps)
 
   def value(self):
     return self._value
+
+  def steps(self):
+    return self._steps
--- tests/test_state.py
+++ tests/test_state.py
@@ -3,6 +3,17 @@ from calcsolver.state import State
 
 class TestState(unittest.TestCase):
 
-  def test_init(self):
+  def test_default_init(self):
+    state = State()
+    self.assertEqual(state.value(), 0)
+    self.assertEqual(state.steps(), [])
+  
+  def test_init_with_value(self):
     state = State(value=10)
     self.assertEqual(state.value(), 10)
+    self.assertEqual(state.steps(), [])
+
+  def test_init_with_steps(self):
+    state = State(steps=['a','b'])
+    self.assertEqual(state.value(), 0)
+    self.assertEqual(state.steps(), ['a', 'b'])
