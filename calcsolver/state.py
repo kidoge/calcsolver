483f9a8879d641047161a324e828db20d59158b6
Kidon Son <kidonson@gmail.com>
2017-08-08 19:37:41 -0400

    Added apply method

--- calcsolver/state.py
+++ calcsolver/state.py
@@ -1,13 +1,21 @@
 class State:
-  _value = 0
-  _steps = []
   
-  def __init__(self, value=0, steps=[]):
+  def __init__(self, value=None, steps=None):
+
+    if value is None:
+      value = 0
+    if steps is None:
+      steps = []
+
     self._value = value
     self._steps = steps
 
   def clone(self):
     return State(_value, _steps)
+
+  def apply(self, operator):
+    self._steps.append(operator)
+    self._value = operator.operate(self)
 
   def value(self):
     return self._value
--- tests/test_state.py
+++ tests/test_state.py
@@ -1,5 +1,8 @@
 import unittest
+from unittest.mock import Mock
+
 from calcsolver.state import State
+
 
 class TestState(unittest.TestCase):
 
@@ -17,3 +20,31 @@ class TestState(unittest.TestCase):
     state = State(steps=['a','b'])
     self.assertEqual(state.value(), 0)
     self.assertEqual(state.steps(), ['a', 'b'])
+
+  def test_apply_once(self):
+    state = State()
+    op = Mock()
+    op.operate = Mock(return_value=3)
+    state.apply(op)
+    op.operate.assert_called_with(state)
+
+    self.assertEqual(state.value(), 3)
+    self.assertEqual(state.steps(), [op])
+
+  def test_apply_twice(self):
+    state = State()
+    op1 = Mock()
+    op1.operate = Mock(return_value=3)
+    state.apply(op1)
+    op1.operate.assert_called_with(state)
+
+    def addTwo(state):
+      return state.value() + 4
+
+    op2 = Mock()
+    op2.operate.side_effect = addTwo
+    state.apply(op2)
+    op2.operate.assert_called_with(state)
+
+    self.assertEqual(state.value(), 7)
+    self.assertEqual(state.steps(), [op1, op2])
