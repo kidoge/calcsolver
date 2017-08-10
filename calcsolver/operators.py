869b287e67f5a4da110a87e6f10cb80e34c3597f
Kidon Son <kidonson@gmail.com>
2017-08-10 18:22:27 -0400

    Fixed format for operators.py.

--- calcsolver/operators.py
+++ calcsolver/operators.py
@@ -1,50 +1,63 @@
+"""This module contains the operators supported by calcsolver."""
+
 import abc
 
-class Operator(object, metaclass=abc.ABCMeta):
-  @abc.abstractmethod
-  def operate(self, state):
-    pass
+class Operator(object, metaclass=abc.ABCMeta): # pylint: disable=too-few-public-methods
+    """Abstract base class for all operators."""
+    @abc.abstractmethod
+    def operate(self, state):
+        """Performs an operation on the passed value"""
+        pass
 
 class AddOperator(Operator):
-  def __init__(self, value):
-    self._value = value
+    """Adds a number."""
+    def __init__(self, value):
+        self._value = value
 
-  def operate(self, state):
-    return state.value() + self._value
+    def operate(self, state):
+        return state.value() + self._value
 
-  def __str__(self):
-    if self._value > 0:
-      sign = "+" 
-    else:
-      sign = "-"
-    return "[ %s %d ]" % (sign, abs(self._value))
+    def __str__(self):
+        if self._value > 0:
+            sign = "+"
+        else:
+            sign = "-"
+        return "[ %s %d ]" % (sign, abs(self._value))
 
-  def value(self):
-    return self._value
+    @property
+    def value(self):
+        """Returns value."""
+        return self._value
 
 class MultiplyOperator(Operator):
-  def __init__(self, value):
-    self._value = value
+    """Multiplies a number."""
+    def __init__(self, value):
+        self._value = value
 
-  def operate(self, state):
-    return state.value() * self._value
+    def operate(self, state):
+        return state.value() * self._value
 
-  def __str__(self):
-    return "[ * %d ]" % self._value
+    def __str__(self):
+        return "[ * %d ]" % self._value
 
-  def value(self):
-    return self._value
+    @property
+    def value(self):
+        """Returns value."""
+        return self._value
 
 class DivideOperator(Operator):
-  def __init__(self, value):
-    self._value = value
+    """Divides a number."""
+    def __init__(self, value):
+        self._value = value
 
-  def operate(self, state):
-    return state.value() / self._value
+    def operate(self, state):
+        return state.value() / self._value
 
-  def __str__(self):
-    return "[ / %d ]" % self._value
+    def __str__(self):
+        return "[ / %d ]" % self._value
 
-  def value(self):
-    return self._value
+    @property
+    def value(self):
+        """Returns value."""
+        return self._value
 
